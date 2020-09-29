1eff7dc3eac98972797e21cadecf6ab8f99744ca
Jaeyeol Lee <rijgndqw012@gmail.com>
2020-09-30 08:45:48 +0900

    2020 여름캠프 내용 반영

--- src/components/molecules/StudyRow.tsx
+++ src/components/molecules/StudyRow.tsx
@@ -1,8 +1,38 @@
 import React from "react"
 import { IStudy } from "@models/study"
 
-const StudyRow: React.FC<{ study: IStudy }> = ({ study }) => (
-  <>
+import styled from "@emotion/styled"
+
+const ContestsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+
+const StudyRow: React.FC<{ study: IStudy }> = ({ study }) => {
+  const renderScoreBoard = (contest) => {
+    const contestKey = study.topic + " " + contest.contest 
+    return (<div key={contestKey}>
+      <p>{contest.contest}</p>
+      <ul>
+        { contest.awards.map(award => 
+            <li key={contestKey + " " + award.prize}>{ award.prize} : { award.member }</li>) }
+      </ul>
+    </div>)
+  }
+
+  const renderContests = (study) => {
+    if (study?.contests?.length >= 1) {
+      return (
+      <ContestsWrapper>
+        { study.contests.map(contest => renderScoreBoard(contest)) }
+      </ContestsWrapper>)
+    } else {
+      return null;
+    }
+  }
+
+  return (<>
     <ul>
         <li>
             스터디 주제 : { study.topic }
@@ -11,7 +41,10 @@ const StudyRow: React.FC<{ study: IStudy }> = ({ study }) => (
             강사 : { study.lecturer }
         </li>
     </ul>
-  </>
-)
+    <div>
+      { renderContests(study) }
+    </div>
+  </>)
+}
 
 export default StudyRow
