056f30fc0f938b2c580fe6f09630a427f5306c67
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-12-04 17:09:52 +0900

    💄 소제목 제작

---
+++ src/components/molecules/title.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import {jsx, css} from '@emotion/core'
+
+type TitleProps={
+    title:string;
+};
+
+const hightlightStyle=css`
+margin-left:2rem;
+padding-left:0.4rem;
+width: 12rem;
+background:linear-gradient(to top, transparent 0%,0%, #33a23d 35%,35%, white 50%);
+border-bottom: 0.3rem solid #33a23d;
+font-size: 2rem;
+font-weight:bold;
+`
+
+export default function StyledTitle(props:TitleProps){
+    return(
+    <div css={hightlightStyle}>
+        {props.title}
+    </div>
+    );
+
+}
