a2e2716e44ca4f0e939f5d7f3e9a67fb53ed7338
malkoG <malkoG@users.noreply.github.com>
2020-12-06 16:29:09 +0000

    Automated deployment: Sun Dec  6 16:29:09 UTC 2020 0ce42548da5ee7a7907241f76f065f0a4570a3c6

--- component---src-pages-404-tsx-c8b1055de1d8138de88e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(t,e,n){"use strict";n.r(e);n("q1tI");var u=n("9Dj+"),o=n("H8eV"),s=n("qKvR");e.default=function(){return Object(s.b)(u.a,null,Object(s.b)(o.a,{title:"404: Not found"}),Object(s.b)("h1",null,"NOT FOUND"),Object(s.b)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-c8b1055de1d8138de88e.js.map
