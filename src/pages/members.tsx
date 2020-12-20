4eed2616cbf72877016b9226c7c6a455a876166d
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-12-20 22:37:42 +0900

    💄 멤버 소개 페이지 작성

---
+++ src/pages/members.tsx
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import React from "react"
+import {jsx,css} from '@emotion/core'
+import { PageProps, Link, graphql } from "gatsby"
+import Layout from "@components/layout"
+import SEO from "@components/seo"
+
+import StyledTitle from "@components/molecules/title.tsx"
+import MemberCard from "@components/organisms/MemberCard"
+import MembersData from "@content/members.yaml"
+
+type DataProps = {
+    site: {
+      buildTime: string
+    }
+  };
+
+const MembersWrapperStyle=css`
+display:flex;
+flex-wrap:wrap;
+justify-content:space-evenly;
+margin-bottom: 8rem;
+`
+
+const Members:React.FC<PageProps<DataProps>> = ({data,path})=>{
+    const renderMember=(member) => <MemberCard position={member.position} image={member.image} name={member.name} school={member.school} address1={member.address1} address2={member.address2}/>
+
+    return(
+        <Layout>
+            <SEO title="ICPC Sinchon - Members"/>
+            <StyledTitle title="Members"/>
+            <div css={MembersWrapperStyle}>
+                {MembersData.content.map(member=>renderMember(member))}
+            </div>
+        </Layout>
+    )
+}
+
+export default Members;
