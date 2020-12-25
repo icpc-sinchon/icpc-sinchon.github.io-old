fea43e40caf7a759e940091f90121b17656e21e6
nant0313 <nant0313@users.noreply.github.com>
2020-12-25 14:13:44 +0000

    Automated deployment: Fri Dec 25 14:13:44 UTC 2020 a3b009b4eb33eec624a93935cb7671b5c6d59100

--- component---src-pages-page-2-tsx-a3d0e78a9dab512491a4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{"+xhR":function(e,t,o){"use strict";o.r(t);o("q1tI");var b=o("Wbzz"),n=o("9Dj+"),c=o("H8eV"),a=o("qKvR");t.default=function(){return Object(a.b)(n.a,null,Object(a.b)(c.a,{title:"Page two"}),Object(a.b)("h1",null,"Hi from the second page"),Object(a.b)("p",null,"Welcome to page 2"),Object(a.b)(b.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-tsx-a3d0e78a9dab512491a4.js.map
