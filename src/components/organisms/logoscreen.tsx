a266104e32576b71f37279e9d14343e02d8d3e53
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-12-27 18:20:10 +0900

    ♻️ 메인 로고 섹션 컴포넌트 분리

---
+++ src/components/organisms/logoscreen.tsx
@@ -0,0 +1,70 @@
+/** @jsx jsx */
+import React,{Component,HTMLAttributes} from "react";
+import {jsx,css} from '@emotion/core';
+
+// images
+import mainImage from "../../images/suapc2020.png"
+
+const logoScreenWrapperStyle = css`
+padding: 7.1rem 1rem;
+margin: 1rem auto 3rem auto;
+background-image: url(${mainImage});
+background-repeat: no-repeat;
+background-position: center center;
+max-width:2210px;
+`
+const bgStyle=css`
+position: relative;
+text-align: right;
+padding-right: 3.3rem;
+`
+const bgLayerStyle = css`
+background-color:rgba(255,255,255,1);
+display: inline-block;
+padding-left: 1rem';
+`
+const logoNumStyle=css`
+color: rgb(0, 132, 13);
+font-family: appleNeoB;
+position: absolute;
+top: 0.9rem;
+right: 2.1rem;
+font-size: 1.5rem;
+`
+const logoTitleStyle=css`
+font-family: segoeuib;
+color: #00840D;
+font-size: 3rem;
+margin: 1.6rem 0;
+`
+const logoContentStyle=css`
+font-family: "TheMpgtM"; 
+font-size: 3.4em; 
+color: black;
+`
+
+export default function LogoScreen(){
+    return(
+    <div className="balloon" css={logoScreenWrapperStyle}>
+        <div className="balloon--text" css={bgStyle}>
+            <div className="balloon--text--whiteback" css={bgLayerStyle}>
+                <div className="balloon--240" css={logoNumStyle}>
+                    240
+                </div>
+                <h1 className="main--title" css={logoTitleStyle}>
+                    ICPC Sinchon
+                </h1>
+                <div css={logoContentStyle}>
+                    <span css={{lineHeight: ``,}}>
+                        신촌지역 대학교
+                    </span>
+                    <br />
+                    <span css={{lineHeight: `5.7rem`,}}>
+                        프로그래밍 대회 동아리 연합
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+} 
