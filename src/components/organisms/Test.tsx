c0a037851f420724d0b925aaa5eb4085de2206ec
dongzoolee <nant0313@gmail.com>
2021-01-01 23:53:17 +0900

    문항선정 배치 수정, 이름 왼쪽 배치 수정

--- src/components/organisms/Test.tsx
+++ src/components/organisms/Test.tsx
@@ -38,6 +38,7 @@ const SunweeCss = css`
 const NameCss = css`
   font-family: "appleNeoM";
   font-size: 1.3rem;
+  text-align: left;
   padding-bottom: 0;
 `
 const ProbPickCss = css`
@@ -45,59 +46,77 @@ const ProbPickCss = css`
   padding-bottom: 0;
   padding-top: 0;
 `
+const StCss=css`
+font-size: 0.7rem;
+`
+const WrapCss = css``
 export default function Test(props: TestProps) {
-  return(
-  <Fragment>
-    {props.contests.map(contest => {
-      return (
-        <div className="test">
-          <table className="rank" css={RankCss}>
-            <caption className="title" css={TitleCss}>
-              {contest.contest_name}
-            </caption>
-            <tbody>
-              {contest.awards.map((award: IAward) => {
-                return (
-                  <tr>
-                    <td className="sunwee" css={SunweeCss}>
-                      {award.prize}
-                    </td>
-                    <td className="name" css={NameCss}>
-                      {award.member}
-                      <span className="school" css={{ fontSize: "0.8rem" }}>
-                        {award.school}
-                      </span>
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-          <table className="ProbPicker--wrap" css={{ display: "inline-block" }}>
-            <caption className="title" css={TitleProbPickCss}>
-              &nbsp;
-            </caption>
-            <tbody>
-              <td className="ProbPick" css={ProbPickCss}>
-                문항선정
-              </td>
-              {contest.problem_picker.map(picker => {
-                return (
-                  <tr>
-                    <td className="name" css={NameCss}>
-                      {picker.name}
-                      <span className="school" css={{ fontSize: "0.8rem" }}>
-                        {picker.school}
-                      </span>
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        </div>
-      )
-    })}
-  </Fragment>
+  return (
+    <Fragment>
+      {props.contests.map(contest => {
+        return (
+          <div className="test">
+            <table className="hof--rank" css={RankCss}>
+              <caption className="title" css={TitleCss}>
+                {contest.contest_name}
+              </caption>
+              <tbody>
+                {contest.awards.map((award: IAward) => {
+                  return (
+                    <tr>
+                      <td className="sunwee" css={SunweeCss}>
+                        {award.prize == 1 ? [1,<span css={StCss}>st</span>] : (award.prize == 2 ? [2,<span css={StCss}>nd</span>] : [3,<span css={StCss}>rd</span>])}
+                      </td>
+                      <td className="name" css={NameCss}>
+                        {award.member}
+                        <span className="school" css={{ fontSize: "0.8rem" }}>
+                          {award.school}
+                        </span>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+            <table
+              className="ProbPicker--wrap"
+              css={{ display: "inline-block" }}
+            >
+              <caption className="title" css={TitleProbPickCss}>
+                &nbsp;
+              </caption>
+              <tbody>
+                {contest.problem_picker.map((picker, key) => {
+                  if (key == 0)
+                    return (
+                      <tr>
+                        <td css={{padding: 0}}>문항선정</td>
+                        <td className="name" css={NameCss}>
+                          {picker.name}
+                          <span className="school" css={{ fontSize: "0.8rem" }}>
+                            {picker.school}
+                          </span>
+                        </td>
+                      </tr>
+                    )
+                  else
+                    return (
+                      <tr>
+                        <td css={{border:"none"}}></td>
+                        <td className="name" css={NameCss}>
+                          {picker.name}
+                          <span className="school" css={{ fontSize: "0.8rem" }}>
+                            {picker.school}
+                          </span>
+                        </td>
+                      </tr>
+                    )
+                })}
+              </tbody>
+            </table>
+          </div>
+        )
+      })}
+    </Fragment>
   )
 }
