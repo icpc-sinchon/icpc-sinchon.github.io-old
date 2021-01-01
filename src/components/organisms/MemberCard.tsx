8b564c31942a58a9b60afb475e5d8ee1351a108f
mori8 <kaithape@gmail.com>
2021-01-02 00:22:11 +0900

    운영진 정보를 년도별로 렌더링하기 위한 컴포넌트 생성, UI 수정

--- src/components/organisms/MemberCard.tsx
+++ src/components/organisms/MemberCard.tsx
@@ -1,9 +1,9 @@
 /** @jsx jsx */
 import React, {Fragment} from "react"
-import {jsx,css} from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faGithub} from "@fortawesome/free-brands-svg-icons"
-import {faAt} from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faAt, faCode } from "@fortawesome/free-solid-svg-icons"
 
 type MemberProps = {
     position: string;
@@ -12,22 +12,22 @@ type MemberProps = {
     school: string;
     address1: string;
     address2: string;
+    address3: string;
 };
 
 const MemberCardStyle=css`
 margin: 1rem 0rem;
-padding: 1rem 4rem 1rem 1rem;
-display:flex;
+padding: 2rem 3rem;
+display: flex;
 align-items:center;
 justify-content:center;
 text-decoration:none;
 color:black;
-border-top:1px solid #707070;
-border-bottom:1px solid #707070;
+text-align: left;
 `
 const imgStyle=css`
-width:220px;
-height:220px;
+width:180px;
+height:180px;
 margin-right: 2rem;
 border-radius: 50%;
 `
@@ -40,11 +40,11 @@ margin-right: 0.3rem;
 
 export default function MemberCard(props:MemberProps){
     return(
-    <a href={props.address2} css={MemberCardStyle}>
+    <a href={"https://www.acmicpc.net/user/" + props.address3} css={MemberCardStyle}>
         <img css={imgStyle} alt={props.name} src={props.image}/>
         <div css={{width:"95%"}}>
-            <div css={{fontSize:"20px", fontWeight:"bold"}}>{props.name}<sub css={{fontWeight:"normal"}}>{props.position}</sub></div>
-            <div css={{fontSize:"14px"}}>{props.school}</div>
+            <div css={{fontSize:"20px", fontWeight:"bold"}}>{props.name}<sub css={{fontWeight:"normal"}}>{props.school}</sub></div>
+            <div css={{fontSize:"14px"}}>{props.position}</div>
             <div css={addrStyle}>
                 <FontAwesomeIcon css={addrIconStyle} icon={faAt} />
                 {props.address1}
@@ -55,6 +55,10 @@ export default function MemberCard(props:MemberProps){
                 {props.address2}
             </div>
             }
+            <div css={addrStyle}>
+                <FontAwesomeIcon css={addrIconStyle} icon={faCode} />
+                {props.address3}
+            </div>
         </div>
     </a>
     );
