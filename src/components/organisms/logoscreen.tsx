30330d92bc2fb1a37db1b0ac3b561b7927b40540
dongzoolee <nant0313@gmail.com>
2021-01-25 17:28:45 +0900

    메인 로고크기 수정

--- src/components/organisms/logoscreen.tsx
+++ src/components/organisms/logoscreen.tsx
@@ -1,74 +1,73 @@
 /** @jsx jsx */
-import React,{Component,HTMLAttributes} from "react";
-import {jsx,css} from '@emotion/core';
+import React, { Component, HTMLAttributes } from "react"
+import { jsx, css } from "@emotion/core"
 
 // images
 import mainImage from "../../images/suapc2020.jpg"
 
 const logoScreenWrapperStyle = css`
-padding: 7.1rem 1rem;
-margin: 1rem auto 3rem auto;
-background-image: url(${mainImage});
-background-repeat: no-repeat;
-background-position: center center;
-max-width:2210px;
+  padding: 7.1rem 1rem;
+  margin: 1rem auto 3rem auto;
+  background-image: url(${mainImage});
+  background-repeat: no-repeat;
+  background-position: center center;
+  max-width: 2210px;
 `
-const bgStyle=css`
-position: relative;
-text-align: right;
-padding-right: 3.3rem;
+const bgStyle = css`
+  position: relative;
+  text-align: right;
+  padding-right: 3.3rem;
 `
 const bgLayerStyle = css`
-background-color:rgba(255,255,255,0.8);
-display: inline-block;
-padding-left: 1rem;
+  background-color: rgba(255, 255, 255, 0.8);
+  display: inline-block;
+  padding-left: 1rem;
 `
-const logoNumStyle=css`
-color: rgb(0, 132, 13);
-font-family: appleNeoB;
-position: absolute;
-top: 0.9rem;
-right: 2.1rem;
-font-size: 1.5rem;
+const logoNumStyle = css`
+  color: rgb(0, 132, 13);
+  font-family: appleNeoB;
+  position: absolute;
+  top: 1rem;
+  right: 2.4rem;
+  font-size: 1.1rem;
 `
-const logoTitleStyle=css`
-font-family: segoeuib;
-color: #00840D;
-font-size: 3rem;
-margin: 1.6rem 0;
+const logoTitleStyle = css`
+  font-family: segoeuib;
+  color: #00840d;
+  font-size: 2.2rem;
+  margin: 1.6rem 0;
 `
-const logoContentStyle=css`
-font-family: "appleNeoR";
-font-size: 3.4em; 
-color: black;
+const logoContentStyle = css`
+  font-family: "appleNeoR";
+  font-size: 3.1em;
+  color: black;
 `
 
-export default function LogoScreen(){
-    return(
+export default function LogoScreen() {
+  return (
     <div className="balloon" css={logoScreenWrapperStyle}>
-        <div className="balloon--text" css={bgStyle}>
-            <div className="balloon--text--whiteback" css={bgLayerStyle}>
-                <div className="balloon--240" css={logoNumStyle}>
-                    240
-                </div>
-                <h1 
-                className="main--title" 
-                css={logoTitleStyle}>
-                    ICPC Sinchon
-                </h1>
-                <div css={logoContentStyle}>
-                    <span className="balloon--title--sin"
-                    css={{lineHeight: ``,}}>
-                        신촌지역 대학교
-                    </span>
-                    <br />
-                    <span className="balloon--title--pmg"
-                    css={{lineHeight: `5.7rem`,}}>
-                        프로그래밍 대회 동아리 연합
-                    </span>
-                </div>
-            </div>
+      <div className="balloon--text" css={bgStyle}>
+        <div className="balloon--text--whiteback" css={bgLayerStyle}>
+          <div className="balloon--240" css={logoNumStyle}>
+            240
+          </div>
+          <h1 className="main--title" css={logoTitleStyle}>
+            ICPC Sinchon
+          </h1>
+          <div css={logoContentStyle}>
+            <span className="balloon--title--sin" css={{ lineHeight: `` }}>
+              신촌지역 대학교
+            </span>
+            <br />
+            <span
+              className="balloon--title--pmg"
+              css={{ lineHeight: `5.7rem` }}
+            >
+              프로그래밍 대회 동아리 연합
+            </span>
+          </div>
         </div>
+      </div>
     </div>
-    )
-} 
+  )
+}
