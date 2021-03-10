fd3ee55af60c5beceb43c2c8fd4612db96861e76
dongzoolee <nant0313@gmail.com>
2021-03-10 19:55:37 +0900

    Fix To Map WinnerTables

--- src/components/organisms/ResultWrapper.tsx
+++ src/components/organisms/ResultWrapper.tsx
@@ -5,25 +5,20 @@ import WinnerTable from "@components/organisms/WinnerTable"
 import OrganizerTable from "@components/organisms/OrganizerTable"
 
 type infoProps = {
-    season: Array<any>
-    seasonName: string
+  season: Array<any>
+  seasonName: string
 }
 
 export default function ResultWrapper(props: infoProps) {
   return (
-    <div className={"result--wrapper "+props.seasonName}>
-      <WinnerTable info={props.season[3][0]} />
-      <WinnerTable info={props.season[3][1]} />
+    <div className={"result--wrapper " + props.seasonName}>
+      {props.season[3].map(val => {
+        return <WinnerTable info={val} />
+      })}
       <div className="organizer--table--wrapper">
-        <OrganizerTable
-          name="출제진"
-          info={props.season[4]}
-        />
+        <OrganizerTable name="출제진" info={props.season[4]} />
         <div className="organizer--table--flexwrap"></div>
-        <OrganizerTable
-          name="검수진"
-          info={props.season[5]}
-        />
+        <OrganizerTable name="검수진" info={props.season[5]} />
       </div>
     </div>
   )
