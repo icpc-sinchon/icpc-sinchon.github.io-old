0ef06ec1a8237faa0d082d5b5fde1bb3ae732db0
dongzoolee <nant0313@gmail.com>
2021-03-16 16:12:22 +0900

    Split Footer Into Components

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import {jsx,css} from '@emotion/core'
 
 import Header from "./header"
-import Address from "./molecules/address"
+import Footer from "./footer"
 import "./layout.css"
 
 type LayoutProps = {
@@ -32,30 +32,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <React.Fragment>
       <Header siteTitle={data.site.siteMetadata.title} />
         <main>{children}</main>
-        <footer >
-          <div className="footer-content">
-            <div className="footer-content--title">ICPC Sinchon</div>
-            <div className="footer-content--items">
-              <Address email="icpc.sinchon@gmail.com" kakao="@icpc_sinchon"/>
-            </div>
-          </div>
-          <div className="footer-content">
-            <div className="footer-content--title">Contributors</div>
-            <div className="footer-content--items">
-              <div className="footer-items--content-column">
-                <Address name="이재열" github="https://github.com/malkoG" email="rijgndqw012@gmail.com"/>
-                <Address name="서연주" github="https://github.com/YeonjuSeo" email="tjduswn1219@gmail.com"/>
-              </div>
-              <div className="footer-items--content-column">
-                <Address name="남수연" github="https://github.com/mori8" email="kaithape@gmail.com"/>
-                <Address name="이동주" github="https://github.com/nant0313" email="nant0313@gmail.com"/>
-              </div>
-            </div>
-          </div>
-          {/* Here is the footer © {new Date().getFullYear()}, Built with
-          {` `} */}
-          {/* <a href="https://www.gatsbyjs.org">Gatsby</a> */}
-        </footer>
+        <Footer />
       </React.Fragment>
   )
 }
